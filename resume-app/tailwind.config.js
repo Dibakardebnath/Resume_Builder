ea9513ca3026e6b71abc83c1b6baf95edb90df2f
Dibakardebnath <ddibakar190@gmail.com>
2024-05-29 00:00:04 +0530

    Versioning untracked files

--- resume-app/tailwind.config.js
+++ resume-app/tailwind.config.js
@@ -1,5 +1,10 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
+  variants: {
+    extend: {
+      display: ['print'],
+    },
+  },
   content: [
     "./pages/**/*.{js,ts,jsx,tsx,mdx}",
     "./components/**/*.{js,ts,jsx,tsx,mdx}",
